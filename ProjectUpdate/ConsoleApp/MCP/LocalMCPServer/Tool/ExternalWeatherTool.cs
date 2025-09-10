68bb565da20b64d09f3c6e8b20ee7db0932a9499
HI_Malwat <220133107022@gecg28.ac.in>
2025-09-10 22:01:53 +0530

    MCP Server with Docker

---
+++ ProjectUpdate/ConsoleApp/MCP/LocalMCPServer/Tool/ExternalWeatherTool.cs
@@ -0,0 +1,40 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+namespace LocalMCPServer.Tool
+{
+    [McpServerToolType]
+    internal class ExternalWeatherTool
+    {
+        private readonly HttpClient _httpClient = new();
+        [McpServerTool, Description("Get the current weather information for a specified city.")]
+        public string? GetWeatherAsync(string cityName)
+        {
+            var apiUrl = $"http://api.weatherapi.com/v1/current.json?key={Config.WeatherApiKey}&q={cityName}&aqi=no";
+            try
+            {
+                HttpResponseMessage response = _httpClient.GetAsync(apiUrl).Result;
+                response.EnsureSuccessStatusCode();
+
+                var responseBody = response.Content.ReadAsStringAsync().Result;
+
+                return responseBody;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error fetching weather data: {ex.Message}");
+                return null;
+            }
+        }
+
+        internal class Config
+        {
+            public static object WeatherApiKey { get; internal set; }
+        }
+    }
+}
