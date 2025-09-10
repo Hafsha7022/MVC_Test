1f3283ff3a236b3c8958d2d1bfb6e6d4e72a62a5
HI_Malwat <220133107022@gecg28.ac.in>
2025-09-10 22:17:59 +0530

    Local MCP Server created
    Local MCP Server created without Docker, with 3 Functions.
    2 functions in BookTool
    1 function in ExternalWeatherTool

---
+++ ProjectUpdate/NewApp_SK/MCP/LocalMCPServer/Tool/ExternalWeatherTool.cs
@@ -0,0 +1,39 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+namespace LocalMCPServer.Tool
+{
+    [McpServerToolType]
+    internal class ExternalWeatherTool
+    {
+        private readonly HttpClient _httpClient = new();
+        // Add a property or field for the WeatherApiKey
+        private readonly string _weatherApiKey;
+        [McpServerTool, Description("Get the current weather information for a specified city.")]
+        public string? GetWeatherAsync(string cityName)
+        {
+            //var apiUrl = $"http://api.weatherapi.com/v1/current.json?key={Config.WeatherApiKey}&q={cityName}&aqi=no";
+            var apiUrl = $"http://api.weatherapi.com/v1/current.json?key={_weatherApiKey}&q={cityName}&aqi=no";
+
+            try
+            {
+                HttpResponseMessage response = _httpClient.GetAsync(apiUrl).Result;
+                response.EnsureSuccessStatusCode();
+
+                var responseBody = response.Content.ReadAsStringAsync().Result;
+
+                return responseBody;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error fetching weather data: {ex.Message}");
+                return null;
+            }
+        }
+    }
+}
