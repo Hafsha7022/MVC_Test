8df07eccea70b884860732324a8070df592b2d54
HI_Malwat <220133107022@gecg28.ac.in>
2025-09-11 12:53:57 +0530

    Change for API Key

--- ProjectUpdate/NewApp_SK/MCP/LocalMCPServer/Tool/ExternalWeatherTool.cs
+++ ProjectUpdate/NewApp_SK/MCP/LocalMCPServer/Tool/ExternalWeatherTool.cs
@@ -12,12 +12,14 @@ namespace LocalMCPServer.Tool
     internal class ExternalWeatherTool
     {
         private readonly HttpClient _httpClient = new();
-        // Add a property or field for the WeatherApiKey
-        private readonly string _weatherApiKey;
+        // Just hard-code here (not recommended for production)
+        private readonly string _weatherApiKey = "your-api-key-here";
+
         [McpServerTool, Description("Get the current weather information for a specified city.")]
         public string? GetWeatherAsync(string cityName)
         {
             //var apiUrl = $"http://api.weatherapi.com/v1/current.json?key={Config.WeatherApiKey}&q={cityName}&aqi=no";
+            //var apiUrl = $"https://wttr.in/{cityName}?format=j1";
             var apiUrl = $"http://api.weatherapi.com/v1/current.json?key={_weatherApiKey}&q={cityName}&aqi=no";
 
             try
